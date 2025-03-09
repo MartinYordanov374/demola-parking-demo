0c4cc45dc775d58ad84f5f638a9500e47b39a823
MartinYordanov374 <martinyordanov284@gmail.com>
2025-03-09 20:16:16 +0200

    open street map

--- src/Components/MapComponent/Map.jsx
+++ src/Components/MapComponent/Map.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import '../../Styles/Map/MapStyles.css'
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
+import { Icon } from 'leaflet'
+import LocationIcon from '../../Icons/dot.svg'
+const LocationMarker = new Icon({
+ iconUrl: LocationIcon,
+ iconSize: [64,64]
+})
+
 export default function Map() {
+  
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (success)=>{setPosition([success.coords.latitude, success.coords.longitude])}, 
+      (err)=>{console.log(err)}
+    )
+  }, [])
+  let [position, setPosition] = useState([61.4466610123261, 23.852789195071736]);
   return (
     <div className='MapWrapper'>
       <div className='MapContainer container'>
-        Map
+        <MapContainer center={position} zoom={20} style={{ height: '100%', width: '100%' }}>
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"> </TileLayer>
+          <Marker position={position} icon={LocationMarker}>
+            <Popup>Your current location</Popup>
+          </Marker>
+        </MapContainer>
       </div>
     </div>
   )
