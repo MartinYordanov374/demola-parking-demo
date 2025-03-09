f2143e97937ebb21a0bce1173c36da41339c1260
MartinYordanov374 <martinyordanov284@gmail.com>
2025-03-09 16:33:40 +0200

    Install and set up react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Components/HomeComponent/Home.jsx';
+import Error from './Components/Error/Error.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route Component={Home} path='/'/>
+        <Route Component={Home} path='/home'/>
+        <Route Component={Error} path='*'/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
