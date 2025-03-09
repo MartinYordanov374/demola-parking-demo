c7dbbf988d13c11fb9e14458e2fea90d530616cb
MartinYordanov374 <martinyordanov284@gmail.com>
2025-03-09 19:18:40 +0200

    Add navigation bar

--- src/Components/NavigationBar/Navigation.jsx
+++ src/Components/NavigationBar/Navigation.jsx
@@ -4,7 +4,17 @@ export default function Navigation() {
   return (
     <div className='NavigationWrapper'>
       <div className='NavigationContainer container'>
-        Navigation
+        <div className='row'>
+          <div className='col-xl-4 NavigationButton'>
+            <i class="bi bi-funnel"></i> Filters
+          </div>
+          <div className='col-xl-4 NavigationButton'>
+            <i class="bi bi-house"></i> Home
+          </div>
+          <div className='col-xl-4 NavigationButton'>
+            <i class="bi bi-gear"></i> Settings
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import '../../Styles/SearchBar/SearchBarStyles.css'
-
+import {InputGroup, FormControl} from 'react-bootstrap'
+import InputGroupText from 'react-bootstrap/esm/InputGroupText'
 export default function SearchBar() {
   return (
-    <div className='wrapper'>
-      <div className='container'>
-        <i className="bi bi-search"></i>
-        <span>Enter an address...</span>
+    <div className='container wrapper'>
+      <div className='container row'>
+        <div className='col-6 SearchBarWrapper'>
+          <InputGroup>
+            <InputGroupText className='SearchIcon'><i className="bi bi-search"/></InputGroupText>
+            <FormControl className='AddressInputField' placeholder='Enter an address...'/>
+          </InputGroup>
+          
+        </div>
+        <div className='col-6 GetLocationWrapper'>
+          <i className="bi bi-geo-alt GetLocationIcon"></i>
+        </div>
+        
       </div>
     </div>
   )
